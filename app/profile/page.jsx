2199c2054e26dfcbc8aeb73be558f1f8c295f848
Dhirajchaudari <dhirajchaudhari789@gmail.com>
2023-07-14 16:15:44 +0530

    added dependancy at profil/page.jsx/useEffec- posts

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,27 +6,27 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
-    const { data: session} = useSession()
-    const router = useRouter()
-    const [posts, setPosts] = useState([])
-    useEffect(() => {
-        const fetchPosts = async () => {
-            const response = await fetch(`/api/users/${session?.user.id}/posts`);
-            const data = await response.json()
-            setPosts(data)
-        }
-        if(session?.user.id) fetchPosts()
-    },[])
-    const handleEdit = (post) => {
-      router.push(`/update-prompt?id=${post._id}`)
+  const { data: session } = useSession()
+  const router = useRouter()
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json()
+      setPosts(data)
     }
+    if (session?.user.id) fetchPosts()
+  }, [posts])
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`)
+  }
 
-    const handleDelete = async (post) => {
-      const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
 
-      if(hasConfirmed) {
+    if (hasConfirmed) {
       try {
-        await fetch(`/api/prompt/${post._id.toString()}`,{
+        await fetch(`/api/prompt/${post._id.toString()}`, {
           method: 'DELETE'
         })
 
@@ -36,19 +36,16 @@ const MyProfile = () => {
       } catch (error) {
         console.log(error)
       }
-      }
-
     }
-
-    
+  }
   return (
     <Profile
-    name="My"
-    desc="Welcome to your personalized profile page"
-    data={posts}
-    handleEdit={handleEdit}
-    handleDelete={handleDelete}
-     />
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
   )
 }
 
